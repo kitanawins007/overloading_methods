c9fb0c16e46abccf38561e5bcab0312c80bb61b7
kitana <126107488+kitanawins007@users.noreply.github.com>
2024-02-14 18:29:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Planta
+{
+    public class Vaso
+    {
+        /*RETORNO DE VALORES
+         * static int Flor(int x)
+        {
+            return 5 + x;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Flor(1));
+        }*/
+
+       /*OVERLOADING
+        * public static int PlusMethodInt(int x, int y)
+        {
+            return x + y;
+        }
+        public static void Main(string[] args)
+        {
+            int orquidea = PlusMethodInt(8, 5);
+            Console.WriteLine(orquidea);
+        }*/
+    }
+}
+
+
